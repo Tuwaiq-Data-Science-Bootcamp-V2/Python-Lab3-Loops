7be91d2e05d842b8a2c56f14c463e9411319418a
SaeedKayy <122199059+SaeedKayy@users.noreply.github.com>
2023-01-11 15:01:38 +0300

    Lab3 updated

--- Lab3Day1.py
+++ Lab3Day1.py
@@ -5,11 +5,20 @@ for i in range(45, 210):
         break
     print(i)
 
-userinput = input("what is the product of 7 * 24 ?")
+userinput = int(input("what is the product of 7 * 24 ?"))
 while userinput == 168:
     print("You answered this Question correctly")
+    break
 else:
     print("Your Answer is wrong try again..")
     userinput = input("what is the product of 7 * 24 ?")
 
 
+def patter(x):
+    for i in reversed(range(1, x + 1)):
+        for j in reversed(range(1, i + 1)):
+            print(j, end=' ')
+        print("")
+
+
+patter(5)
